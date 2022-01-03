9708566af747b603b8fe11b3334a71fc021a6cd7
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2022-01-02 19:56:21 -0500

    Update MintingPolicyTranscribed.hs

--- MintingPolicyTranscribed.hs
+++ MintingPolicyTranscribed.hs
@@ -1,1 +1,16 @@
+import Control.Monad (void)
+import Ledger (Address, ScriptContext)
+import Ledger.Constraints qualified as Constraints
+import Ledger.Typed.Scripts qualified as Scripts
+import Ledger.Value (Value)
+import Ledger.ScriptContext (TxInfo)
+import Playground.Contract
+import Plutus.Contract
+import PlutusTx qualified
+import PlutusTx.Prelude hiding (Applicative (..))
+
+--checks the Script Context to see if the ADA inputed to the transaction is equal to 30 ADA
+{-# INLINABLE mkValidatorForCorrectAmount #-}
+mkValidatorForCorrectAmount :: ()
+mkValidatorForCorrectAmount _ = _ == Ada.lovelaceValueOf 30
 
