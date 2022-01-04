fd3c04ccb8001982c9e84d0b428d1b550882d109
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2022-01-03 21:37:24 -0500

    Update MintingPolicyTranscribed.hs

--- MintingPolicyTranscribed.hs
+++ MintingPolicyTranscribed.hs
@@ -2,15 +2,20 @@ import Control.Monad (void)
 import Ledger (Address, ScriptContext)
 import Ledger.Constraints qualified as Constraints
 import Ledger.Typed.Scripts qualified as Scripts
-import Ledger.Value (Value)
-import Ledger.ScriptContext (TxInfo)
+import Ledger.Value (Value, CurrencySymbol)
 import Playground.Contract
 import Plutus.Contract
 import PlutusTx qualified
 import PlutusTx.Prelude hiding (Applicative (..))
 
---checks the Script Context to see if the ADA inputed to the transaction is equal to 30 ADA
-{-# INLINABLE mkValidatorForCorrectAmount #-}
-mkValidatorForCorrectAmount :: ()
-mkValidatorForCorrectAmount _ = _ == Ada.lovelaceValueOf 30
+--Splits the TxOutValue into its perspective parts so that its useable
+data PaymentParams = PaymentParams 
+    { pCurrencySymbol  :: CurrencySymbol
+      ,pTokenName      :: TokenName
+      ,pAmount         :: Integer
+    } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
+
+-- cheack  the amount of ADA sent by the wallet to makes sure its acceptable
+mkCheckForCorrectAmount :: Integer -> Bool
+mkCheckForCorrectAmount amount = (amount == 30)
