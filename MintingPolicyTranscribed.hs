ed02782345f669c1f8f512d493b9b6a51c1d291d
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2022-01-21 22:36:15 -0500

    Update MintingPolicyTranscribed.hs
    Code to create the base token works int the plutus playground only so change your methods the base for it was the pioneer programs old code so lots of new stuff had to be added and adjusted

--- MintingPolicyTranscribed.hs
+++ MintingPolicyTranscribed.hs
@@ -1,1 +1,74 @@
+import           Control.Monad          hiding (fmap)
+import           Data.Aeson             (ToJSON, FromJSON)
+import           Data.Text              (Text)
+import           Data.Void              (Void)
+import           GHC.Generics           (Generic)
+import           Plutus.Contract        as Contract
+import           Plutus.Trace.Emulator  as Emulator
+import qualified PlutusTx
+import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
+import           Ledger                 hiding (mint, singleton)
+import           Ledger.Constraints     as Constraints
+import qualified Ledger.Typed.Scripts   as Scripts
+import           Ledger.Value           as Value
+import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
+import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types       (KnownCurrency (..))
+import           Prelude                (IO, Show (..), String)
+import           Text.Printf            (printf)
+import           Wallet.Emulator.Wallet
+import           PlutusTx.Prelude
+import           Wallet.Effects (ownPaymentPubKeyHash)
+
+{-# INLINABLE mkPolicy #-}
+mkPolicy :: PubKeyHash -> () -> ScriptContext -> Bool
+mkPolicy pkh () ctx = txSignedBy (scriptContextTxInfo ctx) pkh
+
+policy :: PubKeyHash -> Scripts.MintingPolicy
+policy pkh = mkMintingPolicyScript $
+    $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy . mkPolicy||])
+    `PlutusTx.applyCode`
+    (PlutusTx.liftCode pkh)
+
+curSymbol :: PubKeyHash -> CurrencySymbol
+curSymbol = scriptCurrencySymbol . policy
+
+data MintParams = MintParams
+    { mpTokenName :: !TokenName
+    , mpAmount    :: !Integer
+    } deriving (Generic, ToJSON, FromJSON, ToSchema)
+
+PlutusTx.makeLift ''MintParams
+
+type SignedSchema = Endpoint "mint" MintParams
+
+--there are two error signals so be careful to make sure they dont get triped
+ownPubKey :: PubKeyHash
+ownPubKey = case (toPubKeyHash $ ownAddress ownWallet) of
+    Nothing     ->  error ()
+    Just x      ->  x
+
+ownWallet :: WalletState
+ownWallet = case (emptyWalletState $ knownWallet 1) of 
+    Nothing     ->  error () 
+    Just x      ->  x
+
+mint :: MintParams -> Contract w SignedSchema Text ()
+mint mp = do
+    let pkh = ownPubKey
+        val     = Value.singleton (curSymbol (pkh)) (mpTokenName mp) (mpAmount mp)
+        lookups = Constraints.mintingPolicy $ policy (pkh)
+        tx      = Constraints.mustMintValue val
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    Contract.logInfo @String $ printf "forged %s" (show val)
+
+endpoints :: Contract () SignedSchema Text ()
+endpoints = awaitPromise mint' >> endpoints
+  where
+    mint' = endpoint @"mint" mint
+
+mkSchemaDefinitions ''SignedSchema
+
+mkKnownCurrencies []
 
