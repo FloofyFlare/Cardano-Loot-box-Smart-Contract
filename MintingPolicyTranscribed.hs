66a0e65721ae7eed85641a104adc10a4dec53e93
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2022-01-22 23:20:18 -0500

    Update MintingPolicyTranscribed.hs

--- MintingPolicyTranscribed.hs
+++ MintingPolicyTranscribed.hs
@@ -19,6 +19,50 @@ import           Text.Printf            (printf)
 import           Wallet.Emulator.Wallet
 import           PlutusTx.Prelude
 import           Wallet.Effects (ownPaymentPubKeyHash)
+import qualified PlutusTx.Builtins      as Builtins
+import           Plutus.V1.Ledger.Ada as Ada
+
+--purchase portion of the contract 
+data LootBoxData
+instance Scripts.ValidatorTypes LootBoxData where
+    type instance RedeemerType LootBoxData = ()
+    type instance DatumType LootBoxData = ()
+
+--Checks the validatorHash to make sure the LootBox has tokens to host the transaction
+{-# INLINABLE mkValidator #-}
+mkValidator :: () -> () -> ScriptContext -> Bool
+mkValidator _ _ ctx = True
+
+lootBox :: Scripts.TypedValidator LootBoxData
+lootBox = Scripts.mkTypedValidator @LootBoxData
+    $$(PlutusTx.compile [|| mkValidator ||])
+    $$(PlutusTx.compile [|| wrap ||]) where
+        wrap = Scripts.wrapValidator
+
+valHash :: Ledger.ValidatorHash
+valHash = Scripts.validatorHash lootBox
+
+validate :: Validator
+validate = Scripts.validatorScript lootBox
+
+valAddress :: Address
+valAddress = Scripts.validatorAddress lootBox
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+--Start of minting portion of the smart contract
 
 {-# INLINABLE mkPolicy #-}
 mkPolicy :: PubKeyHash -> () -> ScriptContext -> Bool
@@ -40,9 +84,8 @@ data MintParams = MintParams
 
 PlutusTx.makeLift ''MintParams
 
-type SignedSchema = Endpoint "mint" MintParams
 
---there are two error signals so be careful to make sure they dont get triped
+--there are two error signals triped when wallet 1 isent the owner
 ownPubKey :: PubKeyHash
 ownPubKey = case (toPubKeyHash $ ownAddress ownWallet) of
     Nothing     ->  error ()
@@ -52,6 +95,12 @@ ownWallet :: WalletState
 ownWallet = case (emptyWalletState $ knownWallet 1) of 
     Nothing     ->  error () 
     Just x      ->  x
+
+
+--Start of endpoints
+type SignedSchema = 
+    Endpoint "mint" MintParams
+     .\/ Endpoint "lock" MintParams
 
 mint :: MintParams -> Contract w SignedSchema Text ()
 mint mp = do
@@ -63,10 +112,17 @@ mint mp = do
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
     Contract.logInfo @String $ printf "forged %s" (show val)
 
+lock :: AsContractError e => Promise () SignedSchema e ()
+lock = endpoint @"lock" @MintParams $ \(MintParams name val) -> do
+    let pkh = ownPubKey
+        tx         = Constraints.mustPayToTheScript () $ Value.singleton (curSymbol (pkh)) (mpTokenName) (mpAmount)
+    void (submitTxConstraints lootBox tx)
+
 endpoints :: Contract () SignedSchema Text ()
-endpoints = awaitPromise mint' >> endpoints
+endpoints = awaitPromise (mint' `select` lock') >> endpoints
   where
     mint' = endpoint @"mint" mint
+    lock' = endpoint @"lock" lock
 
 mkSchemaDefinitions ''SignedSchema
 
