16808ce41c25aab54ae0fd61f6228c737b10ab19
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2022-01-23 15:59:46 -0500

    Update MintingPolicyTranscribed.hs

--- MintingPolicyTranscribed.hs
+++ MintingPolicyTranscribed.hs
@@ -11,16 +11,19 @@ import           Ledger                 hiding (mint, singleton)
 import           Ledger.Constraints     as Constraints
 import qualified Ledger.Typed.Scripts   as Scripts
 import           Ledger.Value           as Value
-import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
+import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema) 
 import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions)
 import           Playground.Types       (KnownCurrency (..))
 import           Prelude                (IO, Show (..), String)
 import           Text.Printf            (printf)
-import           Wallet.Emulator.Wallet
+import           Data.Text
+import           Wallet.Emulator.Wallet 
 import           PlutusTx.Prelude
 import           Wallet.Effects (ownPaymentPubKeyHash)
-import qualified PlutusTx.Builtins      as Builtins
 import           Plutus.V1.Ledger.Ada as Ada
+
+price :: Value 
+price = Ada.lovelaceValueOf 5000000
 
 --purchase portion of the contract 
 data LootBoxData
@@ -49,24 +52,19 @@ valAddress :: Address
 valAddress = Scripts.validatorAddress lootBox
 
 
-    
 
 
 
 
 
-
-
-
-
-
-
-
---Start of minting portion of the smart contract
 
 {-# INLINABLE mkPolicy #-}
 mkPolicy :: PubKeyHash -> () -> ScriptContext -> Bool
-mkPolicy pkh () ctx = txSignedBy (scriptContextTxInfo ctx) pkh
+mkPolicy pkh () ctx@ScriptContext{scriptContextTxInfo=txInfo} = 
+    when (price /= (valueSpent txInfo)) $
+        False
+
+    txSignedBy (scriptContextTxInfo ctx) pkh
 
 policy :: PubKeyHash -> Scripts.MintingPolicy
 policy pkh = mkMintingPolicyScript $
@@ -84,8 +82,9 @@ data MintParams = MintParams
 
 PlutusTx.makeLift ''MintParams
 
+type SignedSchema = Endpoint "mint" MintParams
 
---there are two error signals triped when wallet 1 isent the owner
+--there are two error signals so be careful to make sure they dont get triped
 ownPubKey :: PubKeyHash
 ownPubKey = case (toPubKeyHash $ ownAddress ownWallet) of
     Nothing     ->  error ()
@@ -96,33 +95,22 @@ ownWallet = case (emptyWalletState $ knownWallet 1) of
     Nothing     ->  error () 
     Just x      ->  x
 
-
---Start of endpoints
-type SignedSchema = 
-    Endpoint "mint" MintParams
-     .\/ Endpoint "lock" MintParams
-
 mint :: MintParams -> Contract w SignedSchema Text ()
 mint mp = do
     let pkh = ownPubKey
         val     = Value.singleton (curSymbol (pkh)) (mpTokenName mp) (mpAmount mp)
         lookups = Constraints.mintingPolicy $ policy (pkh)
-        tx      = Constraints.mustMintValue val
-    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+        tx1     = Constraints.mustMintValue val 
+        tx2     = Constraints.mustPayToTheScript () price
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx1
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    void (submitTxConstraints lootBox tx2)
     Contract.logInfo @String $ printf "forged %s" (show val)
 
-lock :: AsContractError e => Promise () SignedSchema e ()
-lock = endpoint @"lock" @MintParams $ \(MintParams name val) -> do
-    let pkh = ownPubKey
-        tx         = Constraints.mustPayToTheScript () $ Value.singleton (curSymbol (pkh)) (mpTokenName) (mpAmount)
-    void (submitTxConstraints lootBox tx)
-
 endpoints :: Contract () SignedSchema Text ()
-endpoints = awaitPromise (mint' `select` lock') >> endpoints
+endpoints = awaitPromise mint' >> endpoints
   where
     mint' = endpoint @"mint" mint
-    lock' = endpoint @"lock" lock
 
 mkSchemaDefinitions ''SignedSchema
 
