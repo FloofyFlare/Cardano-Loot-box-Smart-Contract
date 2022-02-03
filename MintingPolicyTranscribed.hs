33001f59ed11c06537bf2548ebf5573a1225daf4
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2022-02-02 20:38:14 -0500

    Update MintingPolicyTranscribed.hs

--- MintingPolicyTranscribed.hs
+++ MintingPolicyTranscribed.hs
@@ -3,6 +3,7 @@ import           Data.Aeson             (ToJSON, FromJSON)
 import           Data.Text              (Text)
 import           Data.Void              (Void)
 import           GHC.Generics           (Generic)
+import           Plutus.Contract        
 import           Plutus.Contract        as Contract
 import           Plutus.Trace.Emulator  as Emulator
 import qualified PlutusTx
@@ -25,13 +26,18 @@ import           Ledger.Tx              (scriptTxOut, ChainIndexTxOut)
 import           Data.Map             as Map
 import           Plutus.ChainIndex.Tx 
 import           Ledger.Blockchain 
+import Playground.Contract
+import           Prelude              ((/), Float, toInteger, floor)
+import           Cardano.Api hiding (Value, TxOut,Address)
+import           Cardano.Api.Shelley hiding (Value, TxOut, Address)
+import           Codec.Serialise hiding (encode)
 
 
 minADA :: Value
 minADA = Ada.lovelaceValueOf 2000000
 
 price :: Value 
-price = Ada.lovelaceValueOf 5000000
+price = Ada.lovelaceValueOf 10000000
 
 data ContractInfo = ContractInfo
     { policyID :: !CurrencySymbol
@@ -50,18 +56,18 @@ contractInfo = ContractInfo
 data LootBoxData
 instance Scripts.ValidatorTypes LootBoxData where
     type instance RedeemerType LootBoxData = ()
-    type instance DatumType LootBoxData = Integer
+    type instance DatumType LootBoxData = ()
 
 --Checks the validatorHash to make sure the LootBox has tokens to host the transaction
 {-# INLINABLE mkValidator #-}
-mkValidator :: Integer -> () -> ScriptContext -> Bool
+mkValidator :: () -> () -> ScriptContext -> Bool
 mkValidator _ _ ctx = True
 
 lootBox :: Scripts.TypedValidator LootBoxData
 lootBox = Scripts.mkTypedValidator @LootBoxData
     $$(PlutusTx.compile [|| mkValidator ||])
     $$(PlutusTx.compile [|| wrap ||]) where
-        wrap = Scripts.wrapValidator @Integer @()
+        wrap = Scripts.wrapValidator
 
 valHash :: Ledger.ValidatorHash
 valHash = Scripts.validatorHash lootBox
@@ -93,59 +99,50 @@ curSymbol = scriptCurrencySymbol . policy
 data MintParams = MintParams
     { mpTokenName :: !TokenName
     , mpAmount    :: !Integer
-    , mpDatum    :: !Integer
     } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
 PlutusTx.makeLift ''MintParams
 
-data LockParams = LockParams
-    { lpTokenName :: !TokenName
-    , lpAmount    :: !Integer
-    , lpDatum    :: !Integer
+data PurchaseParams = PurchaseParams
+    { pTokenName :: !TokenName
+    , pPayment   :: !PaymentPubKeyHash
     } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
-PlutusTx.makeLift ''LockParams
-
---there are two error signals triped when wallet 1 isent the owner
-ownPubKey :: PubKeyHash
-ownPubKey = case (toPubKeyHash $ ownAddress ownWallet) of
-    Nothing     ->  error ()
-    Just x      ->  x
-
-ownWallet :: WalletState
-ownWallet = case (emptyWalletState $ knownWallet 1) of 
-    Nothing     ->  error () 
-    Just x      ->  x
-
+PlutusTx.makeLift ''PurchaseParams
 
 --Start of endpoints
 type SignedSchema = 
     Endpoint "mint" MintParams
-     .\/ Endpoint "lock" LockParams
+     .\/ Endpoint "lock" MintParams
      .\/ Endpoint "purchase" ()
 
-mint :: MintParams -> Contract w SignedSchema Text ()
+mint :: AsContractError e => MintParams -> Contract w s e ()
 mint mp = do
-    let pkh = (walletOwner contractInfo)
-        val     = Value.singleton (curSymbol (pkh)) (mpTokenName mp) (mpAmount mp)
+    ppkh <- Contract.ownPaymentPubKeyHash
+    let pkh     = unPaymentPubKeyHash ppkh
+        val     = Value.singleton (curSymbol pkh) (mpTokenName mp) (mpAmount mp)
         lookups = Constraints.mintingPolicy $ policy (pkh)
         tx      = Constraints.mustMintValue val
     ledgerTx <- submitTxConstraintsWith @Void lookups tx
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-    Contract.logInfo @String $ printf "forged %s" (show val)
+    logInfo @String $ printf "forged %s" (show val)
 
--- makes 1 utxo for the validator script
-lock :: AsContractError e => LockParams -> Contract w s e ()
-lock lp =  do
-    let tx         = Constraints.mustPayToTheScript (lpDatum lp) $ (Value.singleton (policyID contractInfo) (nameOfToken contractInfo) (1)) <> minADA
+lock :: AsContractError e => MintParams -> Contract w s e ()
+lock mp =  do
+    let tx         = Constraints.mustPayToTheScript () $ (Value.singleton (policyID contractInfo) (nameOfToken contractInfo) (mpAmount mp)) <> minADA
     void (submitTxConstraints lootBox tx)
 
-purchase ::  AsContractError e => () -> Contract w s e ()
-purchase p=  do
+purchaseUtxo :: ChainIndexTxOut
+purchaseUtxo = case fromTxOut (scriptTxOut (Value.singleton (policyID contractInfo) (nameOfToken contractInfo) (1)) validate (Datum $ PlutusTx.toBuiltinData ()) )  of
+    Nothing     ->  error ()
+    Just x      ->  x
+
+purchase :: () -> Contract w SignedSchema Text ()
+purchase _ =  do
     utxos <- fundsAtAddressGeq valAddress (Ada.lovelaceValueOf 1)
     let redeemer = () 
-        pkh = ownPubKey
-        tx       = Constraints.mustPayToPubKey (paymentPubKeyHash $ ownPaymentPublicKey ownWallet) price <> collectFromScript utxos redeemer
+        ppkh     = walletOwner contractInfo
+        tx       = mustPayToPubKey (ppkh) price <> collectFromScript utxos redeemer
         
     void (submitTxConstraintsSpending lootBox utxos tx)
 
